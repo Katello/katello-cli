58532ca3b805ed6aab55e1e6549db1a9b7e36116
Lukas Zapletal <lzap+git@redhat.com>
2011-11-10 12:28:54 +0100

    moving system tests into /scripts

---
+++ cli_tests/packagegroup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+header "Package group"
+
+test_failure "list package groups" package_group list --repo_id $REPO_ID
+test_failure "list package group categories" package_group category_list --repo_id $REPO_ID
+
+REPO_ID=$(get_repo_id)
+PACKAGE_GROUP_ID=test
+PACKAGE_GROUP_NAME=test
+PACKAGE_GROUP_CATEGORY_ID=test
+PACKAGE_GROUP_CATEGORY_NAME=test
+create_sample_package_groups
+
+test_success "list package groups" package_group list --repo_id $REPO_ID
+test_success "show package group" package_group info --repo_id $REPO_ID --id $PACKAGE_GROUP_ID
+
+test_success "list package group categories" package_group category_list --repo_id $REPO_ID
+test_success "show pacakge group category" package_group category_info --repo_id $REPO_ID --id $PACKAGE_GROUP_CATEGORY_ID
