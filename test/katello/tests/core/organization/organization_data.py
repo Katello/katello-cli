Katello/katello

large refactor of organization level default system info keys
the purpose of this refactor was to allow for other objects in the
future to have org level default custom info keys.

fixing busted system test

fixing cli nosetests

shortening some lines to make pylint happy

still fixing rspec tests

cli/test/katello/tests/core/organization/organization_data.py
@@ -8,7 +8,7 @@ ORGS = [
     "id": 1,
     "label": "ACME_Corporation",
     "description": "ACME Corporation Organization",
-    "system_info_keys" : ["asset_tag"]
+    "info_keys" : { "system": ["asset_tag"] }
   }
 ]
 

Katello/katello-cli

test/katello/tests/core/organization/organization_data.py
@@ -8,7 +8,7 @@ ORGS = [
     "id": 1,
     "label": "ACME_Corporation",
     "description": "ACME Corporation Organization",
-    "system_info_keys" : ["asset_tag"]
+    "default_info" : { "system": ["asset_tag"] }
   }
 ]
 