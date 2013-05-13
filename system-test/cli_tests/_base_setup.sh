441089b009cf6055a3dc64ea34a9ea589c8f12fe
David Davis <daviddavis@redhat.com>
2013-05-13 11:28:52 -0400

    Revert "Removing legacy promotion path"
    This reverts commit 4f23651490a6b79634c5315e2082ba1ea50a90a5.

--- system-test/cli_tests/_base_setup.sh
+++ system-test/cli_tests/_base_setup.sh
@@ -21,8 +21,6 @@ YUM_PROVIDER="yum_provider_$RAND"
 FEWUPS_REPO_URL="http://lzap.fedorapeople.org/fakerepos/zoo5/"
 FEWUPS_PRODUCT="fewups_product_$RAND"
 FEWUPS_REPO="repo_$RAND"
-TEST_DEF="def_$RAND"
-TEST_VIEW="${TEST_DEF}_view_$RAND"
 # BASIC RESOURCES (reused in tests)
 test_success "user create ($TEST_USER)" user create --username="$TEST_USER" --password=password --email=$TEST_USER@somewhere.com
 test_success "org create ($TEST_ORG)" org create --name="$TEST_ORG" --label="$TEST_ORG_LABEL" --description="org description"
@@ -32,8 +30,5 @@ test_success "environment create ($TEST_ENV_2)" environment create --org="$TEST_
 test_success "provider create ($YUM_PROVIDER)" provider create --name="$YUM_PROVIDER" --org="$TEST_ORG" --url="$FEWUPS_REPO_URL" --description="prov description"
 test_success "product create ($FEWUPS_PRODUCT)" product create --provider="$YUM_PROVIDER" --org="$TEST_ORG" --name="$FEWUPS_PRODUCT" --url="$FEWUPS_REPO_URL" --assumeyes
 test_success "repo create ($FEWUPS_REPO)" repo create --product="$FEWUPS_PRODUCT" --org="$TEST_ORG" --name="$FEWUPS_REPO" --url="$FEWUPS_REPO_URL"
-test_success "content definition create ($TEST_DEF)" content definition create --org="$TEST_ORG" --name="$TEST_DEF"
-test_success "content definition add_repo ($FEWUPS_REPO to $TEST_DEF)" content definition add_repo --org="$TEST_ORG" --name="$TEST_DEF" --product=$FEWUPS_PRODUCT --repo="$FEWUPS_REPO"
-test_success "content definition publish ($TEST_DEF to $TEST_VIEW)" content definition publish --org=$TEST_ORG --label=$TEST_DEF --view_name=$TEST_VIEW
 REPO_NAME=$(get_repo_name)
 REPO_ID=$(get_repo_id)
--- system-test/cli_tests/changeset.sh
+++ system-test/cli_tests/changeset.sh
@@ -11,27 +11,25 @@ CS_NAME="changeset_$RAND"
 CS_NAME_2="changeset_2_$RAND"
 CS_NAME_3="changeset_3_$RAND"
 test_success "changeset create" changeset create --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME"
-test_failure "changeset add product" changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME" --add_product="$FEWUPS_PRODUCT"
+test_success "changeset add product" changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME" --add_product="$FEWUPS_PRODUCT"
 
 check_delayed_jobs_running
 
 test_success "promote changeset with one product" changeset promote --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME"
 
 test_success "changeset create" changeset create --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2"
-test_failure "changeset add package"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --add_package="monkey-0.3-0.8.noarch"
-test_failure "changeset add erratum"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --add_erratum="RHEA-2010:0001"
-test_failure "changeset add repo"     changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --add_repo="$REPO_NAME"
-test_success "changeset add view"     changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --add_content_view="$TEST_VIEW"
+test_success "changeset add package"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --add_package="monkey-0.3-0.8.noarch"
+test_success "changeset add erratum"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --add_erratum="RHEA-2010:0001"
+test_success "changeset add repo"     changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --add_repo="$REPO_NAME"
 
 test_success "changeset promote" changeset promote --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2"
 
 test_success "changeset list" changeset list --org="$TEST_ORG" --environment="$TEST_ENV"
 test_success "changeset info" changeset info --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME"
 
-test_failure "changeset remove product"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME" --remove_product="$FEWUPS_PRODUCT"
-test_failure "changeset remove package"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --remove_package="monkey-0.3-0.8.noarch"
-test_failure "changeset remove erratum"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --remove_erratum="RHEA-2010:0001"
-test_failure "changeset remove repo"     changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --remove_repo="$REPO_NAME"
-test_success "changeset remove view"     changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --remove_content_view_label="$TEST_VIEW"
+test_success "changeset remove product"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME" --remove_product="$FEWUPS_PRODUCT"
+test_success "changeset remove package"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --remove_package="monkey-0.3-0.8.noarch"
+test_success "changeset remove erratum"  changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --remove_erratum="RHEA-2010:0001"
+test_success "changeset remove repo"     changeset update  --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME_2" --from_product="$FEWUPS_PRODUCT" --remove_repo="$REPO_NAME"
 
 test_success "changeset update" changeset update --org="$TEST_ORG" --environment="$TEST_ENV" --name="$CS_NAME" --new_name="new_$CS_NAME" --description="updated description"
