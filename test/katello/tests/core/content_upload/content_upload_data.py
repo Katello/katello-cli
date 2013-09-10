ae2af6a4e01e4939b788874bbab5ccb65f2aebf4
Manjiri Tapaswi <mtapaswi@redhat.com>
2013-09-10 15:48:55 -0400

    Fix CLI tests for package uploads

--- test/katello/tests/core/content_upload/content_upload_data.py
+++ test/katello/tests/core/content_upload/content_upload_data.py
@@ -1,11 +1,20 @@
 CONTENT_UPLOADS = [
     {
         "upload_id": "cfb1fed0-752b-439e-aa68-fba68eababa3",
-        "repo_id": "fedora_17_x86_64",
-        "offset": "0",
-        "filepath": "/tmp/my_file.rpm"
-        "unit_type_id": "rpm",
-        "unit_key": "foo",
-        "unit-metadata" : "bar"
+        "repo": "fedora_17_x86_64",
+        "repo_id": "1",
+        "org": "acme",
+        "product": "rpm",
+        "filepath": "/tmp/bear-4.1-1.noarch.rpm",
+        "content_type": "yum"
+    } ,
+    {
+        "upload_id": "abc123",
+        "repo": "pforge",
+        "repo_id": "5",
+        "org": "acme",
+        "product": "puppet",
+        "filepath": "/tmp/jdob-valid-1.1.0.tar.gz",
+        "content_type": "puppet"
     }
 ]
